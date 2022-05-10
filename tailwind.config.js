ce9ca67be0eedb76d1e8adb2aa07631739f6e113
Rajdeep Singh <radewala.2@gmail.com>
2022-05-10 16:34:42 +0530

    fix issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
+    theme: {extend: {},},
     plugins: [],
-  }
+}
+
+// both work
+// module.exports = {
+//     content: ["./*.{html,js}"],
+//     theme: {extend: {},},
+//     plugins: [],
+// }
+
+// if your file in src folder
+// module.exports = {
+//     content: ["./src/*.{html,js}"],
+//     theme: {extend: {},},
+//     plugins: [],
+// }
